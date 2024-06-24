5ec14e57a22baefffb0f26c5b3a29088ddc67ce5
Alberto Hermosillo <albertoherms17@gmail.com>
2024-06-23 23:28:15 -0600

    fix: adjust animations and types

--- src/components/GiftWithText/GiftWithText.tsx
+++ src/components/GiftWithText/GiftWithText.tsx
@@ -10,17 +10,17 @@ const GifWithText: React.FC<GifWithTextProps> = ({ gifUrl, mainText, secondaryTe
   return (
     <div className="relative w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] flex items-center justify-center mx-auto">
       <img src={gifUrl} alt="GIF" className="w-full h-full object-cover" />
-      <div className="absolute inset-0 flex flex-col items-start md:items-center justify-center px-4 sm:px-8 space-y-4 w-full max-w-7xl mx-auto text-left md:text-center">
-        <div className="flex flex-col items-start md:items-center space-y-4">
-          <h1 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-white leading-tight">
+      <div className="absolute inset-0 flex flex-col justify-center items-start px-4 sm:px-8 space-y-4 w-full max-w-7xl mx-auto text-left">
+        <div className="flex flex-col items-start space-y-4">
+          <h1 className="text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-bold text-white leading-tight text-left">
             {mainText}
           </h1>
         </div>
-        <div className="flex flex-col items-start md:items-center space-y-2">
-          <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white">
+        <div className="flex flex-col items-start space-y-2">
+          <p className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-white text-left">
             {secondaryText}
           </p>
-          <div className="w-24 h-1 bg-blue-600 mt-2"></div>
+          <div className="w-24 sm:w-36 h-1 bg-blue-600 mt-2"></div>
         </div>
       </div>
     </div>
--- src/components/ServicesSection/ServicesSection.tsx
+++ src/components/ServicesSection/ServicesSection.tsx
@@ -1,29 +1,17 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import images from '../../utils/images';
 import { AnimatePresence, motion } from 'framer-motion';
-
-interface Service {
-  name: string;
-  description: string;
-  link: string;
-}
-
-const services: Service[] = [
-  { name: 'Digital talent review', description: 'Ensuring your talent evolves simultaneously is critical in a rapidly changing technological landscape. With our Digital Talent Review, power your digital transformation by understanding your current digital skills, forecasting talent needs, and strategizing your talent roadmap.', link: 'https://emtech.digital/digital_review' },
-  { name: 'Training academies', description: 'EmTech Institute caters to professionals in corporations needing professional development in different technology areas. Our academies are designed to keep pace with the rapid evolution of technology, offering a cutting-edge learning experience.', link: 'https://emtech.digital/digital_review' },
-  { name: 'Centers of excellence', description: 'While technology provides the necessary infrastructure and tools, we offer specialized expertise and strategic guidance to ensure successful implementation, thus maximizing return on investment and positive business impact.', link: 'https://emtech.digital/digital_review' },
-  { name: 'Talent acquisition', description: 'We power technology talent acquisition by blending deep understanding, strategic sourcing, and holistic evaluation; we tailor our services to meet the unique needs of each client, ensuring a perfect fit between the talent and the corporate culture.', link: 'https://emtech.digital/digital_review' },
-  { name: 'Talent ecosystems', description: 'EmTech Institute works with governments, foundations, and universities to empower underserved populations with crucial technology skills, enabling them to access rewarding careers in the tech sector and contribute meaningfully to the competitiveness of their communities.', link: 'https://emtech.digital/digital_review' },
-];
+import { services } from '../../utils/services/services';
+import { Service } from '../../utils/types/Services/ServicesTypes';
 
 const listVariants = {
   hidden: { opacity: 0 },
-  visible: { opacity: 1, transition: { staggerChildren: 0.1 } },
+  visible: { opacity: 1, transition: { staggerChildren: 0.3 } },
 };
 
 const itemVariants = {
   hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
 
 const ServicesSection: React.FC = () => {
@@ -59,14 +47,14 @@ const ServicesSection: React.FC = () => {
         </p>
         <motion.ul
           initial="hidden"
-          animate="visible"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
           variants={listVariants}
           className="space-y-4"
         >
           {services.map((service, index) => (
-            <motion.div key={index}>
+            <motion.div key={index} variants={itemVariants}>
               <motion.li
-                variants={itemVariants}
                 whileHover={{ scale: 1.05, backgroundColor: '#f0f0f0' }}
                 whileTap={{ scale: 1.05, backgroundColor: '#f0f0f0' }}
                 className={`bg-white font-bold text-emtechSecondary rounded-lg shadow-lg p-4 flex justify-between items-center cursor-pointer transition-colors duration-100 ${
@@ -80,22 +68,30 @@ const ServicesSection: React.FC = () => {
                   initial={{ backgroundImage: `url(${images.flecha})` }}
                   animate={{ backgroundImage: selectedService === service && isMobile ? `url(${images.arrowdown})` : `url(${images.flecha})` }}
                   transition={{ duration: 0.3 }}
-                  className="w-6 h-6 bg-no-repeat bg-center"
+                  className="w-6 h-8 bg-no-repeat bg-center"
                 />
               </motion.li>
-              {selectedService === service && isMobile && (
-                <div className="mt-4 text-sm text-white p-4 bg-blue-600 rounded-lg transition-all duration-300 ease-in-out">
-                  <p className="text-justify">{service.description}</p>
-                  <div className="flex justify-center">
-                    <a
-                      href={service.link}
-                      className="mt-4 bg-emtechThird text-white py-3 px-6 rounded"
-                    >
-                      Find out how
-                    </a>
-                  </div>
-                </div>
-              )}
+              <AnimatePresence>
+                {selectedService === service && isMobile && (
+                  <motion.div
+                    initial={{ opacity: 0, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    exit={{ opacity: 0, scale: 0.95 }}
+                    transition={{ duration: 0.2, ease: "easeInOut" }}
+                    className="mt-4 text-sm text-white p-4 bg-blue-600 rounded-lg"
+                  >
+                    <p className="text-justify">{service.description}</p>
+                    <div className="flex justify-center">
+                      <a
+                        href={service.link}
+                        className="mt-4 bg-emtechThird text-white py-3 px-6 rounded"
+                      >
+                        Find out how
+                      </a>
+                    </div>
+                  </motion.div>
+                )}
+              </AnimatePresence>
             </motion.div>
           ))}
         </motion.ul>
@@ -113,13 +109,13 @@ const ServicesSection: React.FC = () => {
                 animate={{ opacity: 1, x: 0 }}
                 exit={{ opacity: 0, x: 100 }}
                 transition={{ duration: 0.6, ease: 'easeInOut' }}
-                className="text-lg mb-4"
+                className="text-lg mt-20"
               >
-                <div className="mb-12 h-1 bg-emtechBlue w-16"></div>
+                <div className="mb-12 h-1 bg-emtechBlue w-44"></div>
                 <p className="text-justify">{selectedService?.description}</p>
                 <a
                   href="https://emtech.digital/digital_review"
-                  className="mt-24 inline-block bg-emtechBlue text-white py-2 px-4 rounded-lg"
+                  className="mt-12 inline-block bg-emtechBlue text-white py-3 px-12 rounded"
                 >
                   Find out how
                 </a>
