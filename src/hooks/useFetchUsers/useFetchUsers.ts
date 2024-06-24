2e443b25ffa05f550c7abcd1587b7a6a362fab14
Alberto Hermosillo <albertoherms17@gmail.com>
2024-06-24 11:51:01 -0600

    fix: updated hook to mantain the refetch on users data

--- src/hooks/useFetchUsers/useFetchUsers.ts
+++ src/hooks/useFetchUsers/useFetchUsers.ts
@@ -1,27 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { User } from '../../utils/types/User/UserTypes';
 
 const useFetchUsers = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://randomuser.me/api/?results=4');
-        setUsers(response.data.results);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    setError(null);
 
-    fetchData();
+    try {
+      const response = await axios.get('https://randomuser.me/api/?results=4');
+      setUsers(response.data.results);
+    } catch (err) {
+      setError('Error fetching data');
+      console.error('Error fetching data:', err);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
-  return { users, loading };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { users, loading, error, refetch: fetchData };
 };
 
 export default useFetchUsers;
